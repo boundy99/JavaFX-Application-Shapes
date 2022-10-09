7f36eb19b982138a23b8ffca74e03b8cb3def6f8
boundy99 <abdoulayeboundydjikine@gmail.com>
2022-10-08 22:21:36 -0400

    Refactor

--- src/main/java/com/project1/MyOval.java
+++ src/main/java/com/project1/MyOval.java
@@ -17,12 +17,14 @@ public class MyOval extends MyShape
     //variables 
     double majorAxis, minorAxis;                 //major and minor axis of the oval
     MyPoint cp;                                  //center point of the oval of type MyPoint
+    MyPoint tp;                                  //top left corner point of the oval of type MyPoint
 
     //constructors
     MyOval(double a, double b, MyPoint tp, MyColor color) 
     {
         super(tp, color);                   //calls the constructor from the super class of MyShape 
-        this.cp = this.p;                   //sets the center point of the oval to p
+        this.tp = this.p;                   //sets top left corner point of the oval to p
+        this.cp = new MyPoint((tp.getXCoordinate()/2), tp.getYCoordinate()/2); //creates a new point calculate the center coordinates
         this.majorAxis = Math.max(a,b);     //initializes the major axis to the bigger value    
         this.minorAxis = Math.min(a,b);     //initializes the minor axis to the smaller value                             
     }
@@ -43,12 +45,15 @@ public class MyOval extends MyShape
      }                                                                 //returns the perimeter of the oval
 
      @Override 
-     public double area() { return 2 * majorAxis * minorAxis; }           //returns the area of the oval
+     public double area() { return 2 * majorAxis * minorAxis; }        //returns the area of the oval
 
      @Override
-    public String toString()                                              //returns the oval's description
+    public String toString()                                           //returns the oval's description
     {
-        return "Center Coordinates : "+
+        return "Point Coordinates : "+
+               "\nX = " + tp.getXCoordinate() + 
+               "\nY = " + tp.getXCoordinate() +
+               "\nCenter Coordinates : "+
                "\nX = " + cp.getXCoordinate() + 
                "\nY = " + cp.getXCoordinate() +
                "\nMajor Axis = " + getA() + 
@@ -61,14 +66,14 @@ public class MyOval extends MyShape
     public void Stroke(GraphicsContext GC)    //draws outline of the oval using JavaFX Application
     {
         GC.setStroke(color.getJavaFXColor());
-        GC.strokeOval(cp.getXCoordinate(), cp.getYCoordinate(), majorAxis, minorAxis);
+        GC.strokeOval(tp.getXCoordinate(), tp.getYCoordinate(), majorAxis, minorAxis);
     }
 
     @Override
     public void draw(GraphicsContext GC)      //fills the interior of the oval using JavaFX Application
     {
         GC.setFill(color.getJavaFXColor());
-        GC.fillOval(cp.getXCoordinate(), cp.getYCoordinate(), majorAxis, minorAxis);
+        GC.fillOval(tp.getXCoordinate(), tp.getYCoordinate(), majorAxis, minorAxis);
     }
 
  } // end of the class MyOval
